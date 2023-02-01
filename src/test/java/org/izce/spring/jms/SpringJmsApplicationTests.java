760fa210e87b2fe4bdc2df245963fb43bbc13d38
izzce <izzetcelik@gmail.com>
2023-02-01 16:14:49 +0300

    initial commit. project setup with Spring Initialzr

---
+++ src/test/java/org/izce/spring/jms/SpringJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.izce.spring.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
