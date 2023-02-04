ca4bfc50eac3e8631fe8c4c31674dc27bb490188
izzce <izzetcelik@gmail.com>
2023-02-04 03:14:56 +0300

    Using local ActiveMQ Broker with Spring Boot. Closes #8

--- src/main/java/org/izce/spring/jms/SpringJmsApplication.java
+++ src/main/java/org/izce/spring/jms/SpringJmsApplication.java
@@ -1,8 +1,5 @@
 package org.izce.spring.jms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,14 +8,14 @@ public class SpringJmsApplication {
 
 	public static void main(String[] args) throws Exception {
 		
-		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-				.setPersistenceEnabled(false)
-				.setJournalDirectory("target/data/journal")
-				.setSecurityEnabled(false)
-				//.addAcceptorConfiguration("invm", "vm://0")
-				.addAcceptorConfiguration("tcp", "tcp://127.0.0.1:61616"));
-
-			server.start();
+//		ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//				.setPersistenceEnabled(false)
+//				.setJournalDirectory("target/data/journal")
+//				.setSecurityEnabled(false)
+//				//.addAcceptorConfiguration("invm", "vm://0")
+//				.addAcceptorConfiguration("tcp", "tcp://127.0.0.1:61616"));
+//
+//			server.start();
 		
 		SpringApplication.run(SpringJmsApplication.class, args);
 	}
